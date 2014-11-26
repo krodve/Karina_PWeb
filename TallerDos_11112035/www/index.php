e5eaf679e199160880226035c470e0c8b5172ae8
krodve <krodve@gmail.com>
2014-11-26 00:52:41 -0500

    taller dos

--- TallerDos_11112035/www/index.php
+++ TallerDos_11112035/www/index.php
@@ -1,7 +1,38 @@
+<!-- conectando PHP -->
+
+<?php 
+require_once('includes/database.php');
+session_start();
+
+//variables de session
+//para saber si esta conectado, 1 o 0
+if(!isset($_SESSION['conconet'])){
+    $_SESSION['conconet']=0;
+}
+
+//pasa el nombre del cliente
+
+if(!isset($_SESSION['nombreUsuario'])){
+$_SESSION['nombreUsuario']="";
+}
+
+//pasa el id del cliente
+if(!isset($_SESSION['ide'])){
+$_SESSION['ide']=0;
+}
+
+//pasa el id del articulo seleccionado
+
+if(!isset($_SESSION['articulo'])){
+$_SESSION['articulo']="";
+}
+?>
+<!-- Fin PHP -->
+
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="utf-8">
+	<meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width">
 	<title>Niponan!</title>
@@ -10,12 +41,39 @@
 	<link href='http://fonts.googleapis.com/css?family=Maven+Pro' rel='stylesheet' type='text/css'>
 </head>
 <body>
+<!-- Inicio PHP -->
+<?php
+
+//toma el id de la variable de sesion
+$id=$_SESSION['ide'];
+
+//si esta conectado entonces dice cuantos items hay en el carrito
+if($_SESSION['conconet']>0){
+$query= "SELECT COUNT(ide) FROM `agregaralcarrito` WHERE compraonline.agregaralcarrito.ideUsuario = $ide ";
+$result= mysqli_query($con,$query);
+$count = mysqli_fetch_array($result);
+	//echo"<a class='carritoH' href='carrito.php'>CARRITO:$count[0] items</a> ";
+}
+
+//toma el nombre de la variable de session
+$nombre= $_SESSION['nombreCliente'];
+
+//si esta conectado un usuario entonces muestra el boton de logout y el nombre, sino muestra login
+if ($_SESSION['contador']>0) {
+echo"<a href='logout.php' class='logout'>LOGOUT</a>"; 
+echo"<a href='perfil.php' class='nombreCliente'>$nombre</a>";
+}else{
+   echo" <a href='login.php?paginaDestino=index.php' class='login'>LOGIN</a> ";
+}
+?>
+<!-- Inicio PHP -->
+
 <header class="headerUno">
     <div class="logo">
         <a href="index.html"><figure class="headerLogo"><img src="images/logo.png"></figure></a>
     </div>
     <div class="inicioSesion">
-        <a href="index.html">Iniciar Sesión</a>
+        <a href="login.php">Iniciar Sesión</a>
 	</div>
 </header>
 
@@ -25,6 +83,16 @@
         <a href="perfil.php">Mi Perfil</a> /
         <a href="catalogo.html">Catalogo</a> /
         <a href="carrito.php">Carrito</a> 
+
+        <?php
+        //si esta conectado entonces al darle en perfil se redigire al login    
+        if($_SESSION['conconet']>0){
+        //echo"<a href='perfil.php'>Perfil</a>"; 
+    	}else{
+        //echo"<a href='login.php?paginaDestino=perfil.php'>Perfil</a>";
+        }        
+        ?>
+
     </nav>
 </div>
 <section>
